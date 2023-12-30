aa95c0dd322347631fa69ff54ecb2933188c0064
NithinYesudas <nithinypadickal@gmail.com>
2023-12-30 23:20:23 +0530

    Added lzw server program

---
+++ lzw_server.py
@@ -0,0 +1,29 @@
+import socket
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server_address = ('127.0.0.1',8000)
+server.bind(server_address)
+server.listen()
+client,address = server.accept()
+msg,address = client.recvfrom(1024)
+msg = msg.decode()
+
+def compress():
+    table = {chr(i):i for i in range(256)}
+    p = msg[0]
+    output_codes = []
+    code = 256
+    for i in range(len(msg)):
+        if i != len(msg) -1:
+            c = msg[i+1]
+        if p+c in table:
+            p = p+c
+        else:
+            output_codes.append(table[p])
+            table[p+c] = code
+            code+=1
+            p = c
+        c=''
+    output_codes.append(table[p])
+    print(output_codes)
+    client.send(output_codes)
+compress()
