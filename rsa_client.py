3cf053a0a4d7504bdca2c5c848879fefd3f722a3
NithinYesudas <nithinypadickal@gmail.com>
2024-01-04 16:57:16 +0530

    fixes

--- rsa_client.py
+++ rsa_client.py
@@ -4,19 +4,26 @@ server_address = ("127.0.0.1",8000)
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client_socket.connect(server_address)
 msg = input("Enter msg to send: ")
-p = 11
-q = 7
-n = p*q
-phi_n = (p-1)*(q-1)
-
-e = 7
-d = pow(e,-1,phi_n)
-encryption_key = pickle.dumps((e,n))
-client_socket.send(encryption_key)
-client_socket.send(msg.encode())
+e=0
+n=0
+def keygeneration():
+    p = 11
+    q = 7
+    n = p*q
+    phi_n = (p-1)*(q-1)
+    e = 7
+    d = pow(e,-1,phi_n)
+def send():
+    encryption_key = pickle.dumps((e,n))
+    client_socket.send(encryption_key)
+    client_socket.send(msg.encode())
 data, address = client_socket.recvfrom(1024)
 encryption_msg = data.decode()
 print(f"Encryption Message: {encryption_msg}")
-decrypted_msg = pow(int(encryption_msg),d,n)
-print(decrypted_msg)
+def decryption():
+    decrypted_msg = pow(int(encryption_msg),d,n)
+    print(decrypted_msg)
+keygeneration()
+send()
+decryption()
 client_socket.close()
