2d6480efd2b1cab72ee542ad3dae3050ca6e232d
NithinYesudas <nithinypadickal@gmail.com>
2024-01-04 17:09:44 +0530

    fixes

--- rsa_client.py
+++ rsa_client.py
@@ -4,26 +4,25 @@ server_address = ("127.0.0.1",8000)
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client_socket.connect(server_address)
 msg = input("Enter msg to send: ")
-e=0
-n=0
-def keygeneration():
-    p = 11
-    q = 7
-    n = p*q
-    phi_n = (p-1)*(q-1)
-    e = 7
-    d = pow(e,-1,phi_n)
-def send():
-    encryption_key = pickle.dumps((e,n))
-    client_socket.send(encryption_key)
-    client_socket.send(msg.encode())
-data, address = client_socket.recvfrom(1024)
+
+#key generation
+p = 11
+q = 7
+n = p*q
+phi_n = (p-1)*(q-1)
+e = 7
+d = pow(e,-1,phi_n)
+
+#send to server
+encryption_key = pickle.dumps((e,n))
+client_socket.send(encryption_key)
+client_socket.send(msg.encode())
+data = client_socket.recv(1024)
 encryption_msg = data.decode()
 print(f"Encryption Message: {encryption_msg}")
-def decryption():
-    decrypted_msg = pow(int(encryption_msg),d,n)
-    print(decrypted_msg)
-keygeneration()
-send()
-decryption()
+
+#decryption
+decrypted_msg = pow(int(encryption_msg),d,n)
+print(f"Decrypted message: {decrypted_msg}")
+
 client_socket.close()
--- rsa_server.py
+++ rsa_server.py
@@ -6,15 +6,17 @@ server_socket.bind(server_address)
 server_socket.listen(5)
 print(f"Server listening on: {server_address}")
 client_socket, client_address = server_socket.accept()
-def encryption():
-    
-    key = client_socket.recv(1024)
-    e,n = pickle.loads(key)
-    msg = client_socket.recv(1024).decode()
-    print(f"Encrypted Message: {msg}")
-    encrypted_msg = pow(int(msg),e,n)
-    print(encrypted_msg)
-    client_socket.send(str(encrypted_msg).encode())
-encryption()
+
+#encryption
+key = client_socket.recv(1024)
+e,n = pickle.loads(key)
+msg = client_socket.recv(1024).decode()
+print(f"to be Encrypted Message: {msg}")
+encrypted_msg = pow(int(msg),e,n)
+print(encrypted_msg)
+
+#send to client
+client_socket.send(str(encrypted_msg).encode())
+
 client_socket.close()
 server_socket.close()
